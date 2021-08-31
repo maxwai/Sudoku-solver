a52a5ca0eb714d0e43d03b55603e056d62d2b6ee
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2021-08-31 22:11:26 +0200

    added posibility to save a sudoku to json and get it back

--- main.py
+++ main.py
@@ -5,5 +5,30 @@ def dummy():
     pass
 
 
+def convert_to_int(text: str) -> int:
+    n: int
+    try:
+        n = int(text)
+    except ValueError:
+        n = 0
+    return n
+
+
+def save_sudoku(button: Gtk.Button):
+    json_dict: dict[str, int] = dict()
+    for position, field in fields.items():
+        json_dict[f'{position.x};{position.y}'] = convert_to_int(field.entry.get_text())
+    with open("saved_sudoku.json", "w") as output_file:
+        json.dump(json_dict, output_file)
+
+
 if __name__ == '__main__':
-    show_window(dummy)
+    label, fields = show_window(solve_sudoku)
+    # label, fields = show_window(save_sudoku)
+    with open("saved_sudoku.json") as intput_file:
+        saved_sudoku: dict[str, int] = json.load(intput_file)
+    for pos, value in saved_sudoku.items():
+        if value == 0:
+            continue
+        fields[Position(int(pos[0]), int(pos[2]))].entry.set_text(str(value))
+    Gtk.main()
